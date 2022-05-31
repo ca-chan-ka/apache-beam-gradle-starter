df4eb932a75b27d83eb27ade8e445f30134959b2
Chan Ka <chan.ka@cloud-ace.jp>
2022-06-01 05:20:21 +0900

    remove personal test data

--- run_dataflow.sh
+++ run_dataflow.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Variables
+gcp_project=
+gcp_temp_location=
+region=
+subnet=
+input_file=
+
 # TBD
 ./gradlew clean execute -Dexec.args="
     --runner=DataflowRunner
-    --gcpTempLocation=gs://lake_biwa/temp_dataflow
-    --project=ca-chan-test
-    --subnetwork=regions/asia-northeast1/subnetworks/vpc-network-main-subnet
-    --inputFile=gs://apache-beam-samples/shakespeare/*
+    --gcpTempLocation=$gcp_temp_location
+    --project=$gcp_project
+    --subnetwork=regions/$region/subnetworks/$subnet
+    --inputFile=$input_file
 "
 
